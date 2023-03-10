39c5589fa1bcd1bea5445971996577e5060cf82f
irayspace <irayspacii@gmail.com>
2023-03-11 04:16:57 +0800

    feat: firestore

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:salon_app/app/pages/discovery/discovery_view.dart';
+import 'package:salon_app/app/pages/startup/startup_view.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
@@ -15,7 +20,7 @@ class MyApp extends StatelessWidget {
       title: 'SalonBeau',
       theme: ThemeData(primarySwatch: Colors.pink),
       debugShowCheckedModeBanner: false,
-      home: const DiscoveryView(),
+      home: const StartupView(),
     );
   }
 }
