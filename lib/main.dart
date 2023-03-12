d64e69e2dcf5b2b544133fdd7edc74877882b152
irayspace <irayspacii@gmail.com>
2023-03-12 12:41:56 +0800

    chore: import

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:salon_app/app/pages/signup/signup_successful.dart';
 import 'package:salon_app/app/pages/startup/startup_view.dart';
 
 import 'firebase_options.dart';
