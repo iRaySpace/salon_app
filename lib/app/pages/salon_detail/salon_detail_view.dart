2ea683e9f380a78b17cf9fe319f4bfc97376a5bb
irayspace <irayspacii@gmail.com>
2023-03-21 11:07:12 +0800

    feat: salon details and image

--- lib/app/pages/salon_detail/salon_detail_view.dart
+++ lib/app/pages/salon_detail/salon_detail_view.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:salon_app/app/pages/appointment_info/appointment_info_view.dart';
 import 'package:salon_app/app/pages/profile/widgets/app_elevated_button.dart';
+import 'package:salon_app/data/salon_repository.dart';
+import 'package:salon_app/data/schedule_repository.dart';
+import 'package:salon_app/domain/entities/salon.dart';
+import 'package:salon_app/domain/entities/schedule.dart';
 
 class SalonDetailView extends StatefulWidget {
   const SalonDetailView({super.key});
@@ -10,6 +15,29 @@ class SalonDetailView extends StatefulWidget {
 }
 
 class _SalonDetailViewState extends State<SalonDetailView> {
+  Salon? _salon;
+  Schedule? _schedule;
+
+  void loadSalon() async {
+    final salon =
+        await SalonRepository().getSalonByUid('hxnx1o1c2xd9u6N3QN7ggp1m1i53');
+    final schedule = await ScheduleRepository().getScheduleByUid(salon.id);
+    setState(() {
+      _salon = salon;
+      _schedule = schedule;
+    });
+  }
+
+  @override
+  void initState() {
+    loadSalon();
+    super.initState();
+  }
+
+  void handleBack() {
+    Navigator.of(context).pop();
+  }
+
   void handleMake() {
     Navigator.push(
       context,
@@ -26,20 +54,50 @@ class _SalonDetailViewState extends State<SalonDetailView> {
         width: double.infinity,
         height: double.infinity,
         color: const Color(0xFFFFD9ED),
+        padding: const EdgeInsets.symmetric(
+          vertical: 25.0,
+          horizontal: 15.0,
+        ),
         child: SafeArea(
           child: SingleChildScrollView(
             child: Column(
               children: [
-                const SizedBox(height: 56.0),
-                const SizedBox(height: 28.0),
+                Row(
+                  children: [
+                    IconButton(
+                      onPressed: handleBack,
+                      icon: const Icon(
+                        Icons.arrow_back,
+                        color: Color(0xFFC93480),
+                        size: 28.0,
+                      ),
+                    ),
+                    Text(
+                      _salon?.salonName ?? 'Salon',
+                      style: const TextStyle(
+                        color: Color(0xFFC93480),
+                        fontWeight: FontWeight.bold,
+                        fontSize: 24.0,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 25.0),
+                if (_salon != null) Image.network(_salon!.logoUrl),
+                const SizedBox(height: 25.0),
                 AppElevatedButton(
                   onPressed: handleMake,
-                  child: Text('Make Appointment'),
+                  child: const Text('Make Appointment'),
                 ),
-                const SizedBox(height: 14.0),
+                const SizedBox(height: 25.0),
                 Container(
                   width: double.infinity,
-                  padding: const EdgeInsets.all(25.0),
+                  padding: const EdgeInsets.only(
+                    top: 20.0,
+                    bottom: 25.0,
+                    left: 25.0,
+                    right: 25.0,
+                  ),
                   decoration: const BoxDecoration(
                     color: Color(0xFFFBADD1),
                     borderRadius: BorderRadius.all(Radius.circular(25.0)),
@@ -54,155 +112,24 @@ class _SalonDetailViewState extends State<SalonDetailView> {
                             style: TextStyle(
                               color: Color(0xFFC93480),
                               fontWeight: FontWeight.bold,
+                              fontSize: 18.0,
                             ),
                           ),
-                          Icon(Icons.schedule),
+                          Icon(
+                            Icons.schedule,
+                            color: Color(0xFFC93480),
+                          ),
                         ],
                       ),
                       const SizedBox(height: 15.0),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: const [
-                          Text(
-                            'Sunday',
-                            style: TextStyle(
-                              color: Color(0xFFC93480),
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          Text(
-                            '8:00 AM - 5:00 PM',
-                            style: TextStyle(
-                              color: Color(0xFFC93480),
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ],
-                      ),
-                      const Divider(height: 10.0),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: const [
-                          Text(
-                            'Monday',
-                            style: TextStyle(
-                              color: Color(0xFFC93480),
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          Text(
-                            '8:00 AM - 5:00 PM',
-                            style: TextStyle(
-                              color: Color(0xFFC93480),
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ],
-                      ),
-                      const Divider(height: 10.0),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: const [
-                          Text(
-                            'Tuesday',
-                            style: TextStyle(
-                              color: Color(0xFFC93480),
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          Text(
-                            '8:00 AM - 5:00 PM',
-                            style: TextStyle(
-                              color: Color(0xFFC93480),
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ],
-                      ),
-                      const Divider(height: 10.0),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: const [
-                          Text(
-                            'Wednesday',
-                            style: TextStyle(
-                              color: Color(0xFFC93480),
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          Text(
-                            '8:00 AM - 5:00 PM',
-                            style: TextStyle(
-                              color: Color(0xFFC93480),
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ],
-                      ),
-                      const Divider(height: 10.0),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: const [
-                          Text(
-                            'Thursday',
-                            style: TextStyle(
-                              color: Color(0xFFC93480),
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          Text(
-                            '8:00 AM - 5:00 PM',
-                            style: TextStyle(
-                              color: Color(0xFFC93480),
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ],
-                      ),
-                      const Divider(height: 10.0),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: const [
-                          Text(
-                            'Friday',
-                            style: TextStyle(
-                              color: Color(0xFFC93480),
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          Text(
-                            '8:00 AM - 5:00 PM',
-                            style: TextStyle(
-                              color: Color(0xFFC93480),
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ],
-                      ),
-                      const Divider(height: 10.0),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: const [
-                          Text(
-                            'Saturday',
-                            style: TextStyle(
-                              color: Color(0xFFC93480),
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          Text(
-                            '8:00 AM - 5:00 PM',
-                            style: TextStyle(
-                              color: Color(0xFFC93480),
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ],
-                      ),
+                      if (_schedule != null)
+                        ..._schedule!.blocks
+                            .map((block) => ScheduleBlockCard(data: block))
+                            .toList()
                     ],
                   ),
                 ),
-                const SizedBox(height: 14.0),
+                const SizedBox(height: 25.0),
                 const Text(
                   'Add Ratings and Feedbacks',
                   style: TextStyle(fontWeight: FontWeight.bold),
@@ -233,5 +160,41 @@ class _SalonDetailViewState extends State<SalonDetailView> {
         ),
       ),
     );
+  }
+}
+
+class ScheduleBlockCard extends StatelessWidget {
+  const ScheduleBlockCard({super.key, required this.data});
+  final ScheduleBlock data;
+  @override
+  Widget build(BuildContext context) {
+    final timeData =
+        '${DateFormat.jm().format(data.startTime)} - ${DateFormat.jm().format(data.endTime)}';
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(15.0),
+      ),
+      child: Container(
+        padding: const EdgeInsets.all(15.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              data.day.capitalize(),
+              style: const TextStyle(
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(timeData),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+extension StringExtension on String {
+  String capitalize() {
+    return "${this[0].toUpperCase()}${this.substring(1).toLowerCase()}";
   }
 }
